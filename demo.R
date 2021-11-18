fc86ce2f05eca9f81a79f6017131075042cfd482
max78 <john.ws.lee@gmail.com>
2021-11-18 12:24:01 -0800

    Finished Ex 6

--- demo.R
+++ demo.R
@@ -12,6 +12,11 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg_new)
+
+main <- function(){
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg_new)
+}
+
+main()
