aab78ccd721675fb247b8e1bcb797b363408f593
max78 <john.ws.lee@gmail.com>
2021-11-18 17:51:26 -0800

    Finished Ex8

--- demo.py
+++ demo.py
@@ -2,10 +2,11 @@
 # date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.py <arg1> [<arg_new>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
+<arg_new>         Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 """ 
@@ -14,4 +15,5 @@ from docopt import docopt
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
+print(opt["<arg_new>"])
 
